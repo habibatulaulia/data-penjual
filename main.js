13b7c947af3af60463987b1daddca001bce495a7
habibatulaulia <habibatulaulia76@gmail.com>
2024-05-06 09:30:20 +0700

    komit semua

---
+++ main.js
@@ -0,0 +1,84 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9uJSx87wLWMehUFmpdboN9sBM9s_tN1M",
+  authDomain: "insan-cemerlang-41b6b.firebaseapp.com",
+  projectId: "insan-cemerlang-41b6b",
+  storageBucket: "insan-cemerlang-41b6b.appspot.com",
+  messagingSenderId: "1057804782651",
+  appId: "1:1057804782651:web:bf911a5c4bff4f7bef201e"
+  
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarPenjual() {
+  const refDokumen = collection(db, "penjual");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      alamat: dok.data().alamat,
+      gmail: dok.data().gmail,
+      noTlpn: dok.data().noTlpn,
+    });
+  });
+  
+  return hasil;
+}
+
+export function formatAngka(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+export async function  tambahPenjual(nama, alamat, gmail, noTlpn) {
+  try {
+    const dokRef = await addDoc(collection(db, 'penjual'), {
+      nama: nama,
+      alamat: alamat,
+      gmail: gmail,
+      noTlpn: noTlpn
+    });
+    console.log('Berhasil menambah penjual' + dokRef.id);
+  } catch (e) {
+    console.log('Gagal menambah penjual' + e);
+  }
+   }
+   
+   export async function hapusPenjual(docId) {
+  await deleteDoc(doc(db, "penjual", docId));
+}
+
+export async function ubahPenjual(docId, nama, alamat, gmail, noTlpn) {
+  await updateDoc(doc(db, "penjual", docId), {
+    nama: nama,
+    alamat: alamat,
+    gmail: gmail,
+    noTlpn: noTlpn
+  });
+}
+
+export async function ambilPenjual(docId) {
+  const docRef = await doc(db, "penjual", docId);
+  const docSnap = await getDoc(docRef);
+
+  return await docSnap.data();
+}
